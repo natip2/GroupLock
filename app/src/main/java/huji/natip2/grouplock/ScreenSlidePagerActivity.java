907a1cc1028703a48623c1c832a301b7427b5be1
natip2 <natip2@gmail.com>
2015-11-12 20:17:12 +0200

    nat; tutorial

--- app/src/main/java/huji/natip2/grouplock/ScreenSlidePagerActivity.java
+++ app/src/main/java/huji/natip2/grouplock/ScreenSlidePagerActivity.java
@@ -28,7 +28,6 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class ScreenSlidePagerActivity extends FragmentActivity implements ActionBar.TabListener {
 
@@ -194,10 +193,24 @@ public class ScreenSlidePagerActivity extends FragmentActivity implements Action
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_section_dummy, container, false);
-            Bundle args = getArguments();
-            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                    getString(R.string.dummy_section_text, args.getInt(ARG_SECTION_NUMBER)));
+            int args = getArguments().getInt(ARG_SECTION_NUMBER);
+            switch (args){
+                case (0):
+
+                    break;
+                case (1):
+
+                    break;
+                case (2):
+
+                    break;
+
+
+            }
+            View rootView = inflater.inflate(R.layout.fragment_image_search, container, false);
+//            Bundle args = getArguments();
+//            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+//                    getString(R.string.dummy_section_text, args.getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
